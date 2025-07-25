a5acf49e1dc00cae347de3d9f94760d43022447b
GermanS98 <germansanchez@outlook.com.ar>
2025-07-25 11:55:13 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,6 +47,7 @@ export default function Home() {
         </div>
       </main>
       <footer className={styles.footer}>
+      <a href="/graficos" style={{ marginRight: '1rem' }}>Gráficos</a>
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
