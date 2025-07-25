9779d2c5dd50dca99a0fbe5634e3ade7c0af52dd
GermanS98 <germansanchez@outlook.com.ar>
2025-07-25 15:27:00 -0300

    Update page.tsx

--- src/app/graficos/page.tsx
+++ src/app/graficos/page.tsx
@@ -12,7 +12,7 @@ export default function Graficos() {
     const interval = setInterval(() => {
       // Forzamos un "reload" cambiando un parámetro dummy
       setUrl(`${baseUrl}&t=${Date.now()}`)
-    }, 100000)
+    }, 60000)
 
     return () => clearInterval(interval)
   }, [])
