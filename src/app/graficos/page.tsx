9e995e1e80c532ff28c56697ec159211b5f554e7
GermanS98 <germansanchez@outlook.com.ar>
2025-07-28 02:49:20 -0300

    Update page.tsx

--- src/app/graficos/page.tsx
+++ src/app/graficos/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 
 export default function Graficos() {
   const baseUrl =
-    'https://app.powerbi.com/reportEmbed?reportId=73116638-db46-4792-bf8d-c634972698b6&autoAuth=true&ctid=e631c664-107d-4e97-b93d-1965130d1449'
+    'https://app.powerbi.com/reportEmbed?reportId=a5cbdf7c-5f35-4335-a145-d7d3c95f212b&autoAuth=true&ctid=e631c664-107d-4e97-b93d-1965130d1449'
 
   const [url, setUrl] = useState(baseUrl)
 
