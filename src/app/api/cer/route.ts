f8a91575ba5a99d1f80cf6e4a52927cb47a119ef
GermanS98 <germansanchez@outlook.com.ar>
2025-07-28 20:37:28 -0300

    Create route.ts

---
+++ src/app/api/cer/route.ts
@@ -0,0 +1,16 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY!
+)
+
+export async function GET() {
+  const { data, error } = await supabase
+    .from('cer')
+    .select('*')
+    .order('fecha', { ascending: true })
+
+  if (error) return new Response(JSON.stringify({ error: error.message }), { status: 500 })
+  return new Response(JSON.stringify(data), { status: 200 })
+}
