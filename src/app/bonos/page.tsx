fb5b2a4b2fe52412187058297c2928dc35c1d721
GermanS98 <germansanchez@outlook.com.ar>
2025-07-28 21:01:01 -0300

    Update page.tsx

--- src/app/bonos/page.tsx
+++ src/app/bonos/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
+import Button from '@/components/ui/button'
 
 export default function Page() {
   const [tickers, setTickers] = useState<{ ticker: string }[]>([])
