b60259246016bf8c1b23da23d7f75023a74fb65d
GermanS98 <germansanchez@outlook.com.ar>
2025-08-30 21:04:25 -0300

    Update route.ts

--- src/app/api/tamar/route.ts
+++ src/app/api/tamar/route.ts
@@ -13,3 +13,4 @@ export async function GET() {
 
   if (error) return new Response(JSON.stringify({ error: error.message }), { status: 500 })
   return new Response(JSON.stringify(data), { status: 200 })
+}
